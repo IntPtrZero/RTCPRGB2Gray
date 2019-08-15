2b7283601f62ebab064b8ccf8f227d4cddbf2572
IntPtr <IntPtrZero@git.com>
2019-08-15 15:09:08 +0800

    release

---
+++ RTCPGrey/RTCPDempo/Main.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Drawing
+Module Main
+
+    Sub Main()
+        Dim sw As New Stopwatch
+        Dim avgtime As UInteger
+        For i = 1 To 5
+            Dim bp As Bitmap = Image.FromFile("test" & i & ".png")
+            sw.Reset()
+            sw.Start()
+            bp = RTCPGray(bp)
+            sw.Stop()
+            avgtime += sw.ElapsedMilliseconds
+            bp.Save("rtcpgray_" & i & ".png")
+            Console.WriteLine("Pic #" & i & ": " & sw.ElapsedMilliseconds.ToString)
+        Next
+        avgtime /= 5
+        Console.WriteLine("Average time: " & sw.ElapsedMilliseconds.ToString)
+        Console.ReadKey()
+    End Sub
+
+End Module
